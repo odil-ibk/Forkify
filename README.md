90e57ffc3b3b4a7ed55955251c716435eed5c50d
odil-ibk <odil.ibk@gmail.com>
2025-05-25 17:09:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# forkify (What Should I Eat?)Project
+
+Recipe application with custom recipe uploads.
---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '136ae178-4a8b-4ca7-bcb1-bdb3b1cfea93';
+export const MODAL_CLOSE_SEC = 2.5;
